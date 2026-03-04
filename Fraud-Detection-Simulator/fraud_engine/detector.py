e9148ef8d7ad85af68a4fd7448747a1bc64b8535
KiraboM <kirabomusoke46@gmail.com>
2026-03-04 13:18:52 +0000

    Changed logic of checkLocation

--- Fraud-Detection-Simulator/fraud_engine/detector.py
+++ Fraud-Detection-Simulator/fraud_engine/detector.py
@@ -29,7 +29,9 @@ def checkLocation(id, total_df, conn):
         next_df = total_df.iloc[i + 1]
         next_id = int(next_df["transaction_id"])
         if current_df["location"] != next_df["location"]:
-            difference = int(next_df["time"]) - int(current_df["time"])
+            num1 = int(next_df["time"])
+            num2 = int(current_df["time"])
+            difference = num1 - num2
             if difference <= 600:
                 flagged_ids.add(current_id)
                 flagged_ids.add(next_id)
